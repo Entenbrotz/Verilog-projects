658f53aa936a036036bd61531909f52f92b20142
R Abhinav <145695800+Entenbrotz@users.noreply.github.com>
2025-01-08 21:09:20 +0530

    Update decodetop.v

--- MIPSprocessor/extratop/decodetop.v
+++ MIPSprocessor/extratop/decodetop.v
@@ -1,51 +1,37 @@
-module topmodule(
-input clk,
-input reset,
-input zero,//imp
-input [31:0]WD,
-input [31:0] instruction,
-output [31:0] rd1,        
-output [31:0] rd2,
-output [31:0] immExt,
-output  PCSrc,
-output  ResultSrc,
-output  MemWrite,
-output  [3:0] ALUControl,
-output  ALUSrc,
-output  [2:0] ImmSrc,
-output  RegWrite
-);
-wire [4:0]rd;
-wire [4:0]rs1;
-wire [4:0]rs2;
-wire [6:0]opcode;
-wire [2:0]funct3;
-wire [6:0]funct7;
-wire [31:0]immv;
 
-InstDecoder(
-    .instruction(instruction),
+
+
+module decodetop(
+    input [31:0]instr,WD,
+    input clk,zero,reset,
+    wire [4:0]rd,rs1,rs2,
+    wire[6:0]opcode,
+    wire[2:0]funct3,
+    wire[6:0]funct7,
+    wire [24:0]immv,
+    output PCSrc,
+    output ResultSrc,
+    output MemWrite,
+    output [2:0]ALUControl,
+    output ALUSrc,
+    wire [2:0] ImmSrc,
+    wire RegWrite,
+    output [31:0]ImmExt,
+    output [31:0] rd1,rd2);
+
+
+InstDecoder instdecode(
+    .instruction(instr),
     .rd(rd),
     .rs1(rs1),
     .rs2(rs2),
     .opcode(opcode),
     .funct3(funct3),
     .funct7(funct7),
-    .immv(immv)  // Sign-extended immediate (32-bits)
-    );
-    
-registerfile (
-    .clk(clk),                // Clock signal
-    .reset(reset),              // Reset signal (active high)
-    .rs1(rs1),          // Read address 1
-    .rs2(rs2),          // Read address 2
-    .rd(rd),           // Write address
-    .WD(WD),          // Write data
-    .write_enable(RegWrite),       // Write enable signal
-    .rd1(rd1),        // Read data 1
-    .rd2(rd2)         // Read data 2
-);
-ControlUnit(
+    .immv(immv));
+
+
+ControlUnit cu(
     .opcode(opcode),
     .funct3(funct3),
     .funct7(funct7),
@@ -58,11 +44,20 @@ ControlUnit(
     .ImmSrc(ImmSrc),
     .RegWrite(RegWrite)
     );
-ImmExtender(
-    .instruction(immv),         // The 32-bit instruction
-    .ImmSrc(ImmSrc),              // Control signal to specify the immediate type
-    .immExt(immExt)         // Extended immediate output
+
+
+ImmExtender imm (
+    .instruction(immv),
+    .ImmSrc(ImmSrc),
+    .immExt(ImmExt)
 );
-  
+
+
+
+registerfile regfile(.clk(clk),
+.reset(reset),.rs1(rs1),.rs2(rs2),.rd(rd),.WD(WD),.write_enable(RegWrite),.rd1(rd1),.rd2(rd2));
+
+
+
 
 endmodule
